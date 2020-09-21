af6c73804daf7e786520a4c8a658251f6c63ce9d
Simo Mafuxwana <simodms@gmail.com>
2020-09-21 15:04:12 +0200

    eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/base',
+    'plugin:prettier/recommended',
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'vue',
+    'prettier'
+  ],
+  // add your custom rules here
+  rules: {
+  }
+}
